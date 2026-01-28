8ef14618b57163de72ffe03172a32eb7aca4ec4c
mayanksrivastava922 <catvideos999yt@gmail.com>
2026-01-28 13:56:38 +0530

    login - logout implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,17 @@
 import express from 'express';
-import dotenv from "dotenv";
 
 import authRoutes from './routes/auth.route.js'
 import { connectDB } from './lib/db.js';
-dotenv.config();
+import { ENV } from './lib/env.js';
 
 const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json()) //req.body
 app.use('/api/auth',authRoutes)
+
+
 app.listen(PORT, () => {
     console.log("server running on PORT" + PORT)
     connectDB();
