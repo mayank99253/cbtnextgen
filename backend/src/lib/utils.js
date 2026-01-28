b7825607a2446e3cf345f47511f3738bb20d2e5f
mayanksrivastava922 <catvideos999yt@gmail.com>
2026-01-28 12:57:17 +0530

    signup code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,18 +1,22 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+    const { JWT_SECRET } = process.env;
+    if (!JWT_SECRET) {
+        throw new Error("JWT_SECRET is not configured");
+    }
+    const token = jwt.sign({ userId }, JWT_SECRET, {
+        expiresIn: "7d",
+    });
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: process.env.NODE_ENV === "development" ? false : true,
-  });
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+        httpOnly: true, // prevent XSS attacks: cross-site scripting
+        sameSite: "strict", // CSRF attacks
+        secure: process.env.NODE_ENV === "development" ? false : true,
+    });
 
-  return token;
+    return token;
 };
 
 // http://localhost
